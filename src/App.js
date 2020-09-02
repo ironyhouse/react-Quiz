958af96ea5ea757d64649759973074d3a4ee2f43
ironyhouse <ironylait@gmail.com>
2020-09-02 20:14:21 +0300

    init project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Layout from './hoc/Layout/Layout'
+import Quiz from './containers/Quiz/Quiz'
+
+function App() {
+  return (
+    <div className="App">
+      <Layout>
+        <Quiz />
+      </Layout>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/containers/Quiz/Quiz.js
@@ -0,0 +1,130 @@
+import React, { Component } from 'react'
+import classes from './Quiz.module.css'
+import ActiveQuiz from '../../components/ActiveQuiz/ActiveOuiz'
+import FinishedQuiz from '../../components/FinishedQuiz/FinishedQuiz'
+
+class Quiz extends Component {
+    state = {
+        results: {
+
+        },
+        isFinished: false,
+        activeQuestion: 0,
+        answerState: null,
+        quiz: [
+            {
+                id: 1,
+                question: 'Какого цвета небо?',
+                rightAnswerId: 2,
+                answers: [
+                    {text: 'Черный', id: 1},
+                    {text: 'Синий', id: 2},
+                    {text: 'Красный', id: 3},
+                    {text: 'Зеленый', id: 4}
+                ]
+            },
+            {
+                id: 2,
+                question: 'Сколько пальцев на руке?',
+                rightAnswerId: 4,
+                answers: [
+                    {text: '2', id: 1},
+                    {text: '3', id: 2},
+                    {text: '4', id: 3},
+                    {text: '5', id: 4}
+                ]
+            }
+        ]
+    }
+
+    onAnswerClickHandler = answerId => {
+        if (this.state.answerState) {
+            const key = Object.keys(this.state.answerState)[0]
+            if (this.state.answerState[key] === 'success') {
+                return
+            }
+        }
+
+        const question = this.state.quiz[this.state.activeQuestion]
+        const results = this.state.results
+
+        if (question.rightAnswerId === answerId) {
+            if (!results[question.id]) {
+                results[question.id] = 'success'
+            }
+
+            this.setState({
+                answerState: {[answerId]: 'success'},
+                results
+            })
+
+            const timeOut = window.setTimeout(() => {
+                if (this.isQuizFinish()) {
+                    this.setState({
+                        isFinished: true
+                    })
+                     
+                
+                } else {
+                    this.setState({
+                        activeQuestion: this.state.activeQuestion + 1,
+                        answerState: null
+                    })
+                }
+
+                window.clearTimeout(timeOut)
+            }, 1000)
+
+        } else {
+            results[question.id] = 'error'
+            this.setState({
+                answerState: {[answerId]: 'error'},
+                results: results
+            })
+        }
+    }
+
+    isQuizFinish() {
+        return this.state.activeQuestion + 1 === this.state.quiz.length
+    }
+
+    retryHandler = () => {
+        this.setState({
+            activeQuestion: 0,
+            answerState: null,
+            isFinished: false, 
+            results: {}
+        })
+    }
+
+    render() {
+        return (
+            <div className={classes.Quiz}>
+
+                <div className={classes.QuizWrapper}>
+                    <h1>Ответьте на все вопросы:</h1>
+
+                    {
+                     this.state.isFinished
+                     ? <FinishedQuiz 
+                            results={this.state.results}
+                            quiz={this.state.quiz}
+                            onRetry={this.retryHandler}
+                        /> 
+                     : <ActiveQuiz 
+                        answers={this.state.quiz[this.state.activeQuestion].answers}
+                        question = {this.state.quiz[this.state.activeQuestion].question}
+                        onAnswerClick = { this.onAnswerClickHandler }
+                        quizLength={this.state.quiz.length}
+                        answerNumber={this.state.activeQuestion + 1}
+                        state={this.state.answerState}
+                    />
+                    }
+                    
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Quiz
