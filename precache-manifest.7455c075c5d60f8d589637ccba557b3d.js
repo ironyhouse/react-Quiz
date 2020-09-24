52b2109e50edee1f695986d6962c8125117547c3
ironyhouse <ironylait@gmail.com>
2020-09-25 01:28:06 +0300

    Updates

---
+++ precache-manifest.7455c075c5d60f8d589637ccba557b3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9306193186b5b5116c7689889b9daf62",
+    "url": "/react-quiz/index.html"
+  },
+  {
+    "revision": "5a3576ea3f6ad27f7490",
+    "url": "/react-quiz/static/css/main.fa4dddc0.chunk.css"
+  },
+  {
+    "revision": "a8685d96cc0c78aa8b79",
+    "url": "/react-quiz/static/js/2.79f0aad2.chunk.js"
+  },
+  {
+    "revision": "fae6e97322ddc0579046aa829b382727",
+    "url": "/react-quiz/static/js/2.79f0aad2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a3576ea3f6ad27f7490",
+    "url": "/react-quiz/static/js/main.10e85c60.chunk.js"
+  },
+  {
+    "revision": "928f8d10e2d93c26f2dd",
+    "url": "/react-quiz/static/js/runtime-main.acabde0b.js"
+  }
+]);
